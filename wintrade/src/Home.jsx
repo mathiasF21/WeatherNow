f1632cb2972b8da2d67949e78ae1dff3a7c02323
Hernan Mathias Farina Forster <hernan.farinaforster@dawsoncollege.qc.ca>
2023-08-19 19:42:06 -0400

    Hooks implemented and basic forecast info displayed with API.

--- wintrade/src/Home.jsx
+++ wintrade/src/Home.jsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+
 function Home() {
-    return <h1>Home Page</h1>;
-  }
-  
-  export default Home;
+  const [data, setData] = useState({
+    temp: 10,
+    city: 'London',
+    humidity: 10,
+    speed: 2  
+  });
+  const API_KEY = "82f396db6021e1881e967d4310ca9fcb"; 
+
+  useEffect(() => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=${API_KEY}&units=metric`;
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL);
+        const jsonData = await response.json();
+
+        if (jsonData.cod === 200) {
+          setData({
+            ...jsonData,
+            temp: jsonData.main.temp,
+            humidity: jsonData.main.humidity,
+            speed: jsonData.wind.speed,
+            city: jsonData.name
+          });
+          console.log(jsonData);
+        } else {
+          console.log("Error fetching data:", jsonData.message);
+        }
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [data.city, API_KEY]); // Include 'data.city' and 'API_KEY' in the dependency array
+
+  return (
+    <h1>Hello {data.city}, the temperature is {data.temp}°C, the humidity is {data.humidity}%, and the wind speed is {data.speed} m/s</h1>
+  );
+}
+
+export default Home;
