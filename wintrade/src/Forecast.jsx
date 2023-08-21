e352f3a67df52089554b0bbd641fc2d1c92a2686
Hernan Mathias Farina Forster <hernan.farinaforster@dawsoncollege.qc.ca>
2023-08-21 03:30:11 -0400

    forecast feature completed.

--- wintrade/src/Forecast.jsx
+++ wintrade/src/Forecast.jsx
@@ -1,28 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-function Forecast({ city }) {
+function Forecast(props) {
 
   const API_KEY = "82f396db6021e1881e967d4310ca9fcb"; 
   const [dayForecast, setDayForecast] = useState([]);
 
-    const fetchForecast = async (city) => {
-      let URL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`;
+  useEffect(() => {
+    const fetchForecast = async () => {
+      let URL = `http://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${API_KEY}&units=metric`;
       try {
         const response = await fetch(URL);
         const jsonData = await response.json();
 
         if(jsonData.cod === "200") {
-          const firstDayForecast = jsonData.list.slice(0,7);
+          const firstDayForecast = jsonData.list.slice(0, 8);
           setDayForecast(firstDayForecast);
         } else {
           console.log("Error fetching data:", jsonData.message);
         }
-      } catch (error) {
+      } catch(error) {
         console.log("Error fetching data:", error);
       }
-    }
+    };
+    fetchForecast(); 
+  }, []);
+    console.log(dayForecast);
     return (
-      <h1>hello</h1>
+      <div className="day-container">
+      {dayForecast.map((forecast, index) => (
+        <Card key={index} className="forecast-card" style={{ width: '18rem' }}>
+        <Card.Img className="card-image" variant="top" src={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}/>
+        <Card.Body>
+          <Card.Title>{forecast.main.temp}°C</Card.Title>
+          <Card.Text>
+            Time: {forecast.dt_txt} <br/>
+            Humidity: {forecast.main.humidity}% <br/>
+            Description: {forecast.weather[0].description} <br/>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      ))}
+    </div>
     );
 }
   
--- wintrade/src/Navbar.jsx
+++ wintrade/src/Navbar.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
 import About from './About';
-import Forecast from './Forecast';
 
 export default function BaseNavbar() {
   return (
@@ -13,16 +12,14 @@ export default function BaseNavbar() {
         <Container>
           <Navbar.Brand as={Link} to="/">WeatherNow</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/forecast">Forecast</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       <Routes>
-        <Route path="/about" element={<About />} />
         <Route path="/" element={<Home />} />
-        <Route path="/forecast" element={<Forecast />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
